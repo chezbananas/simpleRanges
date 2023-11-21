1ae6565fceca79a248837f28beeeab6bdd05b252
Everett Lee <everettlee1@gmail.com>
2023-11-20 16:01:51 -0800

    add stuff for pushing to netlify

--- App.js
+++ App.js
@@ -7,20 +7,20 @@ import {
   Image,
   Dimensions,
   ScrollView,
-} from 'react-native';
-import { useState } from 'react';
-import { AntDesign } from '@expo/vector-icons';
-import Images from './assets/index.js';
+} from "react-native";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import Images from "./assets/index.js";
 
 export default function App() {
   const [currPlayers, setPlayers] = useState(9);
   const [currPosition, setPosition] = useState(1);
   const [currStage, setStage] = useState(0);
-  let stageText = Images[currPosition]['stages'][currStage];
-  let positionText = Images[currPosition]['pos'];
+  let stageText = Images[currPosition]["stages"][currStage];
+  let positionText = Images[currPosition]["pos"];
   const maxStage = Object.keys(Images[currPosition]).length - 3;
   let currImage = Images[currPosition][currStage];
-  const imgWidth = Math.min(Dimensions.get('window').width * 0.9, 400);
+  const imgWidth = Math.min(Dimensions.get("window").width * 0.9, 400);
   const imgHeight = imgWidth * 2.84; // aspect ratio for all images
 
   return (
@@ -36,7 +36,8 @@ export default function App() {
             if (newPlayers < currPosition) {
               setPosition(newPlayers);
             }
-          }}>
+          }}
+        >
           <AntDesign name="minuscircle" size={36} color="black" />
         </Pressable>
         <Text style={styles.playerText}> {currPlayers} </Text>
@@ -57,17 +58,20 @@ export default function App() {
             }
             setPosition(newPos);
             setStage(0);
-          }}>
+          }}
+        >
           <Text style={styles.buttonText}> Prev Position </Text>
         </Pressable>
         <Pressable
           style={styles.buttonStyle}
-          onPress={() => setStage(Math.max(currStage - 1, 0))}>
+          onPress={() => setStage(Math.max(currStage - 1, 0))}
+        >
           <Text style={styles.buttonText}> Prev Stage </Text>
         </Pressable>
         <Pressable
           style={styles.buttonStyle}
-          onPress= {() => setStage(Math.min(currStage + 1, maxStage))}          >
+          onPress={() => setStage(Math.min(currStage + 1, maxStage))}
+        >
           <Text style={styles.buttonText}> Next Stage </Text>
         </Pressable>
         <Pressable
@@ -79,7 +83,8 @@ export default function App() {
             }
             setPosition(newPos);
             setStage(0);
-          }}>
+          }}
+        >
           <Text style={styles.buttonText}>Next Position</Text>
         </Pressable>
       </SafeAreaView>
@@ -96,44 +101,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
     padding: 8,
-    alignItems: 'center',
+    alignItems: "center",
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: '2%',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: "2%",
+    width: "100%",
   },
   playerText: {
     fontSize: 30,
   },
   positionContainer: {
-    width: '25%',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "25%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   positionText: {
     fontSize: 12,
   },
   secondHeader: {
-    height: '5%',
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    height: "5%",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
   },
   buttonStyle: {
-    backgroundColor: 'green',
-    borderRadius: '25%',
-    justifyContent: 'center',
-    textAlign: 'center',
-    alignItems: 'center',
-    width: '23%',
+    backgroundColor: "green",
+    borderRadius: "25%",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
+    width: "23%",
   },
 });
