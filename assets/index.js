086ca51cef30198a47630d0b750d77408f4545e5
Everett Lee <everettlee1@gmail.com>
2023-12-07 15:13:10 -0800

    reimport assets

---
+++ assets/index.js
@@ -0,0 +1,75 @@
+const Images = {
+  1: {
+    // bb
+    0: require("./bb/0.jpg"),
+    pos: "BB",
+    stages: ["Facing RFI"],
+  },
+  2: {
+    // sb
+    0: require("./sb/0.jpg"),
+    1: require("./sb/1.jpg"),
+    2: require("./sb/2.jpg"),
+    pos: "SB",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  3: {
+    // btn
+    0: require("./btn/0.jpg"),
+    1: require("./btn/1.jpg"),
+    2: require("./btn/2.jpg"),
+    pos: "BTN",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  4: {
+    // co
+    0: require("./co/0.jpg"),
+    1: require("./co/1.jpg"),
+    2: require("./co/2.jpg"),
+    pos: "CO",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  5: {
+    // hj
+    0: require("./hj/0.jpg"),
+    1: require("./hj/1.jpg"),
+    2: require("./hj/2.jpg"),
+    pos: "HJ",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  6: {
+    // lj
+    0: require("./lj/0.jpg"),
+    1: require("./lj/1.jpg"),
+    2: require("./lj/2.jpg"),
+    pos: "LJ",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  7: {
+    // utg2
+    0: require("./utg2/0.jpg"),
+    1: require("./utg2/1.jpg"),
+    2: require("./utg2/2.jpg"),
+    pos: "UTG+2",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  8: {
+    // utg1
+    0: require("./utg1/0.jpg"),
+    1: require("./utg1/1.jpg"),
+    2: require("./utg1/2.jpg"),
+    pos: "UTG+1",
+    stages: ["RFI", "Facing RFI", "RFI vs 3Bet"],
+  },
+  9: {
+    // utg
+    0: require("./utg/0.jpg"),
+    1: require("./utg/1.jpg"),
+    pos: "UTG",
+    stages: ["RFI", "RFI vs 3Bet"],
+  },
+  cards: require("./cards.png"),
+  dice: require("./dice.png"),
+};
+
+export default Images;
